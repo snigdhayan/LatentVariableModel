c008e503e89c5e6f535f2be9e656f6c576815fd0
snigdhayan <snigdhayan@gmail.com>
2020-07-05 14:30:19 +0200

    Added accuracy calculation

--- BreastCancerDataAnalysisViaLVM.R
+++ BreastCancerDataAnalysisViaLVM.R
@@ -2,7 +2,7 @@
 library(lavaan)
 
 # specify model - predict model via two latent variables; the latent variables are modeled according to clusters of feature agglomeration
-breast_cancer_model <- ' label ~ latent_var1 + latent_var2
+model <- ' label ~ latent_var1 + latent_var2
               latent_var1 =~ worst_area + worst_perimeter + worst_radius + area_error + perimeter_error 
                             + mean_radius + mean_perimeter + radius_error + mean_area  
               latent_var2 =~ mean_fractal_dimension + worst_smoothness + mean_smoothness + worst_fractal_dimension 
@@ -15,7 +15,7 @@ data_train <- read.csv(file = 'normalized_breast_cancer_dataset_train.csv', head
 # data <- data[names(data)!='label']
 
 # fit model
-fit <- cfa(breast_cancer_model, data=data_train, std.lv=TRUE, missing="fiml", control=list(iter.max=1000))
+fit <- cfa(model, data=data_train, std.lv=TRUE, missing="fiml", control=list(iter.max=1000))
 
 # display summary output
 # summary(fit, fit.measures=TRUE)
@@ -34,3 +34,4 @@ myFn <- function(x) {
 pred_binary <- apply(pred["label"], 1, FUN = myFn)
 isCorrect <- pred_binary == data_test["label"]
 table(isCorrect)
+print(paste0("Accuracy = ", round(sum(isCorrect,na.rm = TRUE)/length(isCorrect)*100,2)))
