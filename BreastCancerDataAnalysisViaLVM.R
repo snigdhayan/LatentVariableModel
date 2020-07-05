310eeb657aff0d9647adb415caef6ae6c4813ee4
snigdhayan <snigdhayan@gmail.com>
2020-07-05 16:19:55 +0200

    Added variances and covariances to model; scripted train-test split

--- BreastCancerDataAnalysisViaLVM.R
+++ BreastCancerDataAnalysisViaLVM.R
@@ -2,17 +2,33 @@
 library(lavaan)
 
 # specify model based on two latent variables - the latent variables are measured by the features in the clusters of feature agglomeration
-model <- ' label ~ latent_var1 + latent_var2
+model <- ' # regression
+              label ~ latent_var1 + latent_var2
+           # latent variables
               latent_var1 =~ worst_area + worst_perimeter + worst_radius + area_error + perimeter_error 
                             + mean_radius + mean_perimeter + radius_error + mean_area  
               latent_var2 =~ mean_fractal_dimension + worst_smoothness + mean_smoothness + worst_fractal_dimension 
-                            + mean_symmetry + worst_symmetry '
+                            + mean_symmetry + worst_symmetry 
+           # variances and covariances
+              worst_perimeter ~~ mean_perimeter 
+              worst_radius ~~ mean_radius
+              worst_area ~~ mean_area
+              worst_smoothness ~~ mean_smoothness 
+              mean_symmetry ~~ worst_symmetry 
+              mean_fractal_dimension ~~ worst_fractal_dimension '
               
               
 # read data
 setwd('/Users/ibatu/Documents/MyProgramsWindows/R/LatentVariableModel/')
-data_train <- read.csv(file = 'normalized_breast_cancer_dataset_train.csv', header = TRUE)
+data <- read.csv(file = 'normalized_breast_cancer_dataset.csv', header = TRUE)
 # data <- data[names(data)!='label']
+
+library(caTools)
+set.seed(101) 
+split_ratio = 0.7
+sample = sample.split(data, SplitRatio = split_ratio)
+data_train = subset(data, sample == TRUE)
+data_test  = subset(data, sample == FALSE)
 
 # fit model
 fit <- cfa(model, data=data_train, std.lv=TRUE, missing="fiml", control=list(iter.max=1000))
@@ -22,7 +38,7 @@ fit <- cfa(model, data=data_train, std.lv=TRUE, missing="fiml", control=list(ite
 
 # Predict on test dataset
 
-data_test <- read.csv('normalized_breast_cancer_dataset_test.csv')
+# data_test <- read.csv('normalized_breast_cancer_dataset_test.csv')
 pred <- as.data.frame(lavPredict(fit, type = "ov", newdata = data_test))
 
 # Check prediction accuracy
